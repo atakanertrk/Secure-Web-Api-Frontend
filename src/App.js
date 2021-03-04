f272be962b4ab8118affcaa7f47b78b44f634ce8
atakan <atakanertrk@hotmail.com>
2021-03-05 00:19:12 +0300

    hashing, bootstrap and some configurations added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import React from 'react';
 import axios from 'axios'
 import sha256 from 'crypto-js/sha256';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+import Base64 from 'crypto-js/enc-base64';
 import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
 import CreateOrder from './components/CreateOrder';
+import constants from './constants.json';
+import { HmacSHA256 } from 'crypto-js';
 
 class App extends React.Component {
   // sha256(nonce + message);
@@ -18,18 +22,23 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.setState({
+      token: read_cookie("token")
+    });
+  }
+
   loginOnClick = async (paramater) => {
     var un = document.getElementById("username").value;
     var pw = document.getElementById("password").value;
+    var hashedInfo = sha256(un + pw).toString();
+    console.log(hashedInfo);
     var LoginObject = {
-      UserName: un,
-      Password: pw
+      hashedUserNameAndPassword: hashedInfo
     }
-    console.log(LoginObject);
-    await axios.post(`https://server.webde.biz.tr/api/login`, LoginObject)
+    await axios.post(`https://${constants.url}/api/login`, LoginObject)
       .then(res => {
         bake_cookie('token', res.data.token);
-        console.log(res.data.token);
         this.setState({
           token: res.data.token
         })
@@ -38,53 +47,62 @@ class App extends React.Component {
         this.setState({
           token: ""
         })
+        alert("login failed");
       });
   }
 
   createAccountOnClick = async (paramater) => {
     var un = document.getElementById("username").value;
     var pw = document.getElementById("password").value;
-    var createAccObj = {
-      UserName: un,
-      Password: pw
+    var hashedInfo = sha256(un + pw).toString();
+    if (un != "" && pw != "") {
+      var createAccObj = {
+        UserName: un,
+        HashedUserNameAndPassword: hashedInfo
+      }
+      console.log(createAccObj);
+      await axios.post(`https://${constants.url}/api/signin`, createAccObj)
+        .then(res => {
+          bake_cookie('token', res.data.token);
+          this.setState({
+            token: res.data.token
+          })
+        }).catch(err => {
+          console.log(err);
+          this.setState({
+            token: ""
+          })
+          alert("create failed");
+        });
     }
-    console.log(createAccObj);
-    await axios.post(`https://server.webde.biz.tr/api/signin`, createAccObj)
-      .then(res => {
-        bake_cookie('token', res.data.token);
-        console.log(res.data.token);
-        this.setState({
-          token: res.data.token
-        })
-      }).catch(err => {
-        console.log(err);
-        this.setState({
-          token: ""
-        })
-      });
   }
 
   render() {
-    if(this.state.token != ""){
-      return(
+    if (this.state.token != "") {
+      return (
         <div>
-          <CreateOrder/>
+          <CreateOrder />
         </div>
       );
     }
     return (
       <div className="App">
-        <p>User Name</p>
-        <input type="text" id="username" autoComplete="off" required />
-        <p>Password</p>
-        <input type="text" id="password" autoComplete="off" required />
-        <hr style={{ width: "100px" }} />
-        <br />
-        <button onClick={() => { this.loginOnClick("hello") }} >Login</button>
-        <br />
-        <button onClick={() => { this.createAccountOnClick("hello") }} >Create Account</button>
-        <br/>
-        <p>current token is : {this.state.token}</p>
+        <h4 style={{marginTop:"10%"}}>Secure Web Application</h4>
+        <div style={{ width: "50%", marginLeft: "25%", marginTop: "4%" }}>
+          <div className="form-group">
+            <label>User Name</label>
+            <input type="text" class="form-control" placeholder="user name" id="username" autoComplete="off" />
+            <small class="form-text text-muted">your security is important to us !</small>
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" placeholder="Password" id="password" autoComplete="off" />
+          </div>
+          <hr style={{ width: "10px" }} />
+          <button type="button" class="btn btn-primary" onClick={() => { this.loginOnClick("hello") }}>Login</button>
+          <hr style={{ width: "100px" }} />
+          <button type="button" class="btn btn-secondary" onClick={() => { this.createAccountOnClick("hello") }}>Create Account</button>
+        </div>
       </div>
     );
   }
